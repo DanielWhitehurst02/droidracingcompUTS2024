6acab558970eca498b2aede55a728f5ff590c1ca
Daniel <dwhitehurst18@gmail.com>
2024-04-30 15:36:27 +1000

    Thresholding and Contour tests

--- src/cv_bridge.cpp
+++ src/cv_bridge.cpp
@@ -6,7 +6,10 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <vector> 
 
+using namespace cv;
+using namespace std;
 
 static const std::string OPENCV_WINDOW = "Image window";
 
@@ -17,6 +20,8 @@ class ImageConverter
   image_transport::Subscriber image_sub_;
   image_transport::Publisher image_pub_;
 
+  cv::Mat R;
+  cv::Mat L;
 
 public:
   ImageConverter()
@@ -49,20 +54,23 @@ public:
     }
 
     // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(200, 50), 10, CV_RGB(255,0,0));
+    //if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+      //cv::circle(cv_ptr->image, cv::Point(200, 50), 10, CV_RGB(255,0,0));
 
     // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    //cv::imshow(OPENCV_WINDOW, cv_ptr->image);
     cv::waitKey(3);
 
     //cv::Size s = cv_ptr->image.size();
     
-    cv::Mat A = cv_ptr->image.clone();
-    cv::Mat L = A(cv::Range(0,480), cv::Range(0,640));
-    cv::Mat R = A(cv::Range(0,480), cv::Range(640,1280));
+    cv::Mat A ;//= cv_ptr->image.clone();
+    cv::flip(cv_ptr->image.clone(),A,-1);
+    L = A(cv::Range(0,480), cv::Range(0,640)); //left
+    R = A(cv::Range(0,480), cv::Range(640,1280)); //right
+    //v::Mat Lflip ;
+    //cv::flip(L, Lflip, 0);
 
-
+    //cv::imshow(OPENCV_WINDOW, A);
     //cv::imshow(OPENCV_WINDOW, L);
     //cv::imshow(OPENCV_WINDOW, R);
 
@@ -75,12 +83,211 @@ public:
 
     
   }
+
+  void Getstereo(cv::Mat &left, cv::Mat &right)
+  {
+    left = L;
+    right = R;
+  }
 };
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
-  ros::spin();
-  return 0;
+// int main(int argc, char** argv)
+// {
+//   ros::init(argc, argv, "image_converter");
+//   ImageConverter ic;
+//   ros::spin();
+  
+//   cv::Mat right;
+//   cv::Mat left;
+
+//   namedWindow("Control"); //create a window called "Control"
+
+
+//  int iLowH = 0;
+//  int iHighH = 179;
+
+//  int iLowS = 0; 
+//  int iHighS = 255;
+
+//  int iLowV = 0;
+//  int iHighV = 255;
+
+//  //Create trackbars in "Control" window
+//  createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+//  createTrackbar("HighH", "Control", &iHighH, 179);
+
+//  createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+//  createTrackbar("HighS", "Control", &iHighS, 255);
+
+//  createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+//  createTrackbar("HighV", "Control", &iHighV, 255);
+
+//   while (true)
+//   {
+    
+//     ic.Getstereo(left, right);
+//     //     bool bSuccess = right; // read a new frame from video
+
+//     //      if (!bSuccess) //if not success, break loop
+//     //     {
+//     //          cout << "Cannot read a frame from video stream" << endl;
+//     //          break;
+//     //     }
+
+//     Mat imgHSV;
+
+//     cvtColor(right, imgHSV, COLOR_RGB2HSV); //Convert the captured frame from BGR to HSV
+ 
+//     Mat imgThresholded;
+
+//     inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+      
+//     //morphological opening (remove small objects from the foreground)
+//     erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+//     dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+
+//     //morphological closing (fill small holes in the foreground)
+//     dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+//     erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+//     imshow("Thresholded Image", imgThresholded); //show the thresholded image
+//     imshow("Original", right); //show the original image
+
+//         if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+//        {
+//             cout << "esc key is pressed by user" << endl;
+//             break; 
+//        }
+//   }
+
+
+//   return 0;
+// }
+
+ int main( int argc, char** argv )
+ {
+    VideoCapture cap(1); //capture the video from web cam
+
+    if ( !cap.isOpened() )  // if not success, exit program
+    {
+         cout << "Cannot open the web cam" << endl;
+         return -1;
+    }
+
+    namedWindow("Control"); //create a window called "Control"
+
+ int iLowH = 0;
+ int iHighH = 255;
+
+ int iLowS = 0; 
+ int iHighS = 255;
+
+ int iLowV = 0;
+ int iHighV = 255;
+
+ //Create trackbars in "Control" window
+ createTrackbar("LowH", "Control", &iLowH, 255); //Hue (0 - 179)
+ createTrackbar("HighH", "Control", &iHighH, 255);
+
+ createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+ createTrackbar("HighS", "Control", &iHighS, 255);
+
+ createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+ createTrackbar("HighV", "Control", &iHighV, 255);
+
+    while (true)
+    {
+        //Mat imgOriginal;
+
+        //bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+
+        //Mat temp;
+
+       
+        Mat temp; //(cv::Range(0,480), cv::Range(0,640)); //left
+        cap >> temp;
+
+        
+
+        flip(temp ,temp,-1);
+
+        Mat leftcap = temp(cv::Range(0,240), cv::Range(0,320)); //left
+        //Mat rightcap = temp(cv::Range(0,480), cv::Range(640,1280)); //right
+
+        //  if (!bSuccess) //if not success, break loop
+        // {
+        //      cout << "Cannot read a frame from video stream" << endl;
+        //      break;
+        // }
+
+  Mat imgHSV;
+
+  cvtColor(leftcap, imgHSV, COLOR_RGB2HSV); //Convert the captured frame from BGR to HSV
+ 
+  Mat imgThresholdedYellow;
+  Mat imgThresholdedBlue;
+
+  inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholdedYellow); //Threshold the image yellow
+  //inRange(imgHSV, Scalar(9, iLowS, iLowV), Scalar(18, iHighS, iHighV), imgThresholdedBlue); //Threshold the blue
+      
+  //morphological opening (remove small objects from the foreground)
+  erode(imgThresholdedYellow, imgThresholdedYellow, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+  dilate( imgThresholdedYellow, imgThresholdedYellow, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+
+  // erode(imgThresholdedBlue, imgThresholdedBlue, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+  // dilate( imgThresholdedBlue, imgThresholdedBlue, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+
+  //morphological closing (fill small holes in the foreground)
+  dilate( imgThresholdedYellow, imgThresholdedYellow, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+  erode(imgThresholdedYellow, imgThresholdedYellow, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+  // dilate( imgThresholdedBlue, imgThresholdedBlue, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+  // erode(imgThresholdedBlue, imgThresholdedBlue, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+
+  // for (int i = 0; i < imgThresholdedYellow.rows; i++ ){
+  //   for (int j = 0; j < imgThresholdedYellow.cols; j++){
+
+
+  //     cout << imgThresholdedYellow.at<double>(j, i) << " ";
+
+  //   }
+  //   cout << endl;
+  // }
+
+  // cv::MatIterator_<double> _it = imgThresholdedYellow.begin<double>();
+  // for(;_it!=imgThresholdedYellow.end<double>(); _it++){
+  //   std::cout << *_it << std::endl;
+  // }
+
+  //cout << imgThresholdedYellow.cols << endl;
+
+  vector<vector<Point>> contours;
+  vector<Vec4i> hierarchy;
+  findContours(imgThresholdedYellow, contours, hierarchy, RETR_TREE, CHAIN_APPROX_NONE);
+
+  // draw contours on the original image
+  Mat image_copy = leftcap.clone();
+  drawContours(image_copy, contours, -1, Scalar(0, 255, 0), 2);
+  imshow("None approximation", image_copy);
+
+  imshow("Thresholded Image Yellow", imgThresholdedYellow); //show the thresholded image
+  // imshow("Thresholded Image Blue", imgThresholdedBlue); //show the thresholded image
+  imshow("Original", leftcap); //show the original image
+
+
+        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+       {
+            cout << "esc key is pressed by user" << endl;
+            break; 
+       }
+
+       
+
+    }
+
+
+   return 0;
+
 }
+
